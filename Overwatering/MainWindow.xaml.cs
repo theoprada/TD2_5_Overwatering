dcf98c5daa5d50d13e66aa18c7fb9010f61d3425
Theo Prada <Theo.Prada@etu.univ-smb.fr>
2025-12-10 11:46:46 +0100

    Menu Jeu et fonctionnement boutons

--- Overwatering/MainWindow.xaml.cs
+++ Overwatering/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿+using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,16 +13,34 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SaeBase
+namespace Overwatering
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour UCMenuJeu.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            AfficheJeu();
+        }
+
+        private void AfficheJeu()
+        {
+            // crée et charge l'écran de démarrage
+            UCJeu uc = new UCJeu();
+
+            // associe l'écran au conteneur
+            ZoneJeu.Content = uc;
+            uc.butDemarrer.Click += AfficherChoixPerso;
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window1 parametreWindow = new Window1();
+            bool? rep = parametreWindow.ShowDialog();
         }
     }
 }
